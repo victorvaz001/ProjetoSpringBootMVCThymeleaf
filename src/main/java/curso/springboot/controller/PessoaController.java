545a9089ba7dc0ca687fcab97e34ca1ea36132ce
Victor Gonçalves <victorvaz001@gmail.com>
2020-06-06 17:56:46 -0300

    Carregando lista de usuarios, via GET

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
 import curso.springboot.repository.PessoaRepository;
@@ -23,6 +24,18 @@ public class PessoaController {
 	public String salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
 		return "cadastro/cadastropessoa";
+	}
+	//ModelAndView -> ligar o modelo de dados cadatrados no banco com a tela(view)
+	@RequestMapping(method = RequestMethod.GET, value = "/listapessoas")
+	public ModelAndView pessoas() {
+		
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		
+		//pessoas -> objeto vindo da view
+		andView.addObject("pessoas", pessoasIt);
+		
+		return andView;
 	}
 
 }
