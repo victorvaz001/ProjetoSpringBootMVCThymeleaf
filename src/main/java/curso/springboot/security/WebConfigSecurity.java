acf2abf67d3b6769ad8e3109bd8a8d4329effd16
Victor Gonçalves <victorvaz001@gmail.com>
2020-06-09 13:35:38 -0300

    Criptografando a senha do usuário

--- src/main/java/curso/springboot/security/WebConfigSecurity.java
+++ src/main/java/curso/springboot/security/WebConfigSecurity.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -28,9 +28,9 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Override //Cria autenticação do usuário com banco de dados ou em memória
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+		auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
 		.withUser("victor")
-		.password("123")
+		.password("$2a$10$73fPlhqqkPMf8m62P/OVx.e4G9JewGXi5AlOYnQKDkjkzE7hkA8QK")
 		.roles("ADMIN");
 	}
 	
