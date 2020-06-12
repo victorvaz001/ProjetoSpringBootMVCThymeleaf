1e96e581e4b678ae7cea4add334082f77c0d2c37
Victor Gonçalves <victorvaz001@gmail.com>
2020-06-12 17:53:04 -0300

    Carregando combo com ENUM

--- src/main/java/curso/springboot/model/Pessoa.java
+++ src/main/java/curso/springboot/model/Pessoa.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -49,6 +51,17 @@ public class Pessoa implements Serializable {
 	
 	@ManyToOne
 	private Profissao profissao;
+	
+	@Enumerated(EnumType.STRING)
+	private Cargo cargo;
+	
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	
+	public Cargo getCargo() {
+		return cargo;
+	}
 	
 	public void setProfissao(Profissao profissao) {
 		this.profissao = profissao;
